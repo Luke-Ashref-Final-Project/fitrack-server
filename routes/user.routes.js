797c48368d97bf2ce3ad7ce3e0e81701ea819501
Achref LAABIDI <abidi.achref95@gmail.com>
2023-08-25 23:35:01 +0100

    added public folder

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,6 @@ const { isAuthenticated }= require('../middleware/jwt.middleware')
 const { getUserProfile } = require('../controllers/userController');
 
 router.get("/profile", isAuthenticated, getUserProfile );
-
+// can export the payload and import it here, create an endpoint in react to handle it
 
 module.exports = router;
