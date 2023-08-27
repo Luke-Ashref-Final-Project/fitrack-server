c5adbdb062d28187d295035fe66f15c9ca4c0bbb
Luke Chen <heylukechen@gmail.com>
2023-08-27 12:49:20 +0200

    make the getallsubscribers route work for coach

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { updatePassword } = require('../controllers/userController');
+const { updatePassword, getAllSubscribers } = require('../controllers/userController');
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
-router.put("/profile/password",isAuthenticated, updatePassword);
+router.put("/profile/password", isAuthenticated, updatePassword);
+router.get("/profile/getallsubscribers", isAuthenticated, getAllSubscribers);
 
 module.exports = router;
