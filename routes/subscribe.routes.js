2b5262ffefdbd11f03637ada689f65f8a5e887d2
Achref LAABIDI <abidi.achref95@gmail.com>
2023-08-31 13:16:49 +0100

    unsub feature now works

--- routes/subscribe.routes.js
+++ routes/subscribe.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { subscribe } = require("../controllers/subscribeController");
+const { subscribe, unSubscribe } = require("../controllers/subscribeController");
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 router.post("/subscribe/:coachId", isAuthenticated, subscribe)
+router.put("/unsubscribe/:coachId", isAuthenticated, unSubscribe)
 
 module.exports = router;
