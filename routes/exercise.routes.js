1684c280fbf7a0e15ddcca2bdff3129ba689e5d2
Achref LAABIDI <abidi.achref95@gmail.com>
2023-09-02 13:45:12 +0100

    minor fixes

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createNewExercise, viewExercisesOfCoach} = require("../Controllers/exerciseController");
+const { createNewExercise, viewExercisesOfCoach} = require("../controllers/exerciseController");
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 router.post("/exercise/new", createNewExercise);
